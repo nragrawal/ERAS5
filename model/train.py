2d3a0bc697423378b9bcae598c572b6f4e03633d
Nishant Agrawal <agrawalnr07@gmail.com>
2024-11-22 00:18:21 -0800

    Working model 1

--- model/train.py
+++ model/train.py
@@ -8,16 +8,23 @@ import os
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3)  # 28x28 -> 26x26
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3)  # 26x26 -> 24x24
-        self.fc1 = nn.Linear(16 * 24 * 24, 128)
-        self.fc2 = nn.Linear(128, 10)
+        # First conv: 28x28 -> 26x26
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=3)
+        # Pool: 26x26 -> 13x13
+        # Second conv: 13x13 -> 11x11
+        self.conv2 = nn.Conv2d(10, 10, kernel_size=3)
+        # Pool: 11x11 -> 5x5 (rounded down from 5.5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.2)
+        # Therefore final feature map is 8 channels of 5x5
+        self.fc1 = nn.Linear(10 * 5 * 5, 64)
+        self.fc2 = nn.Linear(64, 10)
         
     def forward(self, x):
-        x = torch.relu(self.conv1(x))
-        x = torch.relu(self.conv2(x))
-        x = x.view(-1, 16 * 24 * 24)
-        x = torch.relu(self.fc1(x))
+        x = self.pool(torch.relu(self.conv1(x)))  # Conv -> ReLU -> Pool
+        x = self.pool(torch.relu(self.conv2(x)))  # Conv -> ReLU -> Pool
+        x = x.view(-1, 10 * 5 * 5)  # Flatten: 8 channels * 5 * 5
+        x = self.dropout(torch.relu(self.fc1(x)))
         x = self.fc2(x)
         return x
 
@@ -32,12 +39,12 @@ def train():
     ])
     
     train_dataset = datasets.MNIST('data', train=True, download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
     
     # Initialize model
     model = SimpleCNN().to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters())
+    optimizer = optim.Adam(model.parameters(), lr=0.003)
     
     # Training loop
     model.train()
